55873e905c66b1ec1f750cfec1ed98f81b7797f6
magicalbeauty91 <magicalbeauty91@gmail.com>
2022-03-16 15:35:03 -0500

    added an extra message with a link to the additional issues on github

--- assets/js/single.js
+++ assets/js/single.js
@@ -2,6 +2,7 @@ var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
+
 var getRepoIssues = function(repo) {
   // format the github api url
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -13,6 +14,11 @@ var getRepoIssues = function(repo) {
       response.json().then(function(data) {
         // pass response data to dom function
         displayIssues(data);
+
+          // check if api has paginated issues
+    if (response.headers.get("Link")) {
+      displayWarning(repo);
+    }
       });
     }
     else {
@@ -57,5 +63,20 @@ if (issues[i].pull_request) {
 issueEl.appendChild(typeEl);
       }
 };
+
+
+var displayWarning = function(repo) {
+  // add text to warning container
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+  var linkEl = document.createElement("a");
+  linkEl.textContent = "See More Issues on GitHub.com";
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("target", "_blank");
+
+  // append to warning container
+  limitWarningEl.appendChild(linkEl);
+};
+
 
 getRepoIssues("facebook/react");
